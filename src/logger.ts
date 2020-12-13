cb6d37da079372917962b6753cfdedd244c76405
Yogendra Singh <yog27ray@gmail.com>
2020-12-13 06:37:55 +0530

    fix: sub-module change application name

---
+++ src/logger.ts
@@ -0,0 +1,68 @@
+import debug, { Debugger } from 'debug';
+
+export const enum LogSeverity {
+    VERBOSE = 'verbose',
+    INFO = 'info',
+    WARN = 'warn',
+    DEBUG = 'debug',
+    ERROR = 'error',
+}
+
+const LogLevel: { [key in LogSeverity]: number } = {
+    verbose: 1,
+    info: 2,
+    warn: 3,
+    debug: 4,
+    error: 5,
+};
+
+export class Logger {
+    private static LOG_LEVEL_ENABLED: Array<LogSeverity> = [
+        LogSeverity.VERBOSE,
+        LogSeverity.INFO,
+        LogSeverity.WARN,
+        LogSeverity.DEBUG,
+        LogSeverity.ERROR,
+    ].filter((logLevel: LogSeverity) => (LogLevel[process.env.DEBUG_LEVEL] || LogLevel[LogSeverity.DEBUG]) <= LogLevel[logLevel]);
+
+    private readonly _debugLogger: Debugger;
+
+    private static isLogEnabled(logSeverity: LogSeverity): boolean {
+        return Logger.LOG_LEVEL_ENABLED.includes(logSeverity);
+    }
+
+    get debugLogger(): Debugger {
+        return this._debugLogger;
+    }
+
+    verbose(formatter: unknown, ...args: Array<unknown>): void {
+        this.log(LogSeverity.VERBOSE, formatter, ...args);
+    }
+
+    info(formatter: unknown, ...args: Array<unknown>): void {
+        this.log(LogSeverity.INFO, formatter, ...args);
+    }
+
+    warn(formatter: unknown, ...args: Array<unknown>): void {
+        this.log(LogSeverity.WARN, formatter, ...args);
+    }
+
+    debug(formatter: unknown, ...args: Array<unknown>): void {
+        this.log(LogSeverity.DEBUG, formatter, ...args);
+    }
+
+    error(formatter: unknown, ...args: Array<unknown>): void {
+        this.log(LogSeverity.ERROR, formatter, ...args);
+    }
+
+    log(logSeverity: LogSeverity, formatter: unknown, ...args: Array<unknown>): void {
+        if (!Logger.isLogEnabled(logSeverity)) {
+            return;
+        }
+        this._debugLogger(`${logSeverity} ${formatter}`, ...args);
+    }
+
+    constructor(name: string) {
+        this._debugLogger = debug(name);
+    }
+}
--- src/logger4-node.ts
+++ src/logger4-node.ts
@@ -1,78 +1,13 @@
-import debug, { Debugger } from 'debug';
-
-export const enum LogSeverity {
-  VERBOSE = 'verbose',
-  INFO = 'info',
-  WARN = 'warn',
-  DEBUG = 'debug',
-  ERROR = 'error',
-}
-
-const LogLevel: { [key in LogSeverity]: number } = {
-  verbose: 1,
-  info: 2,
-  warn: 3,
-  debug: 4,
-  error: 5,
-};
+import { Logger } from './logger';
 
 export class Logger4Node {
-  private static _ApplicationName: string = '';
+  private readonly _applicationName: string;
 
-  private static LOG_LEVEL_ENABLED: Array<LogSeverity> = [
-    LogSeverity.VERBOSE,
-    LogSeverity.INFO,
-    LogSeverity.WARN,
-    LogSeverity.DEBUG,
-    LogSeverity.ERROR,
-  ].filter((logLevel: LogSeverity) => (LogLevel[process.env.DEBUG_LEVEL] || LogLevel[LogSeverity.DEBUG]) <= LogLevel[logLevel]);
-
-  private readonly _debugLogger: Debugger;
-
-  static create(name: string): Logger4Node {
-    return new Logger4Node(`${Logger4Node._ApplicationName}:${name}`);
+  constructor(applicationName: string) {
+    this._applicationName = applicationName;
   }
 
-  static setApplicationName(applicationName: string): void {
-    Logger4Node._ApplicationName = applicationName;
-  }
-
-  private static isLogEnabled(logSeverity: LogSeverity): boolean {
-    return Logger4Node.LOG_LEVEL_ENABLED.includes(logSeverity);
-  }
-
-  get debugLogger(): Debugger {
-    return this._debugLogger;
-  }
-
-  verbose(formatter: unknown, ...args: Array<unknown>): void {
-    this.log(LogSeverity.VERBOSE, formatter, ...args);
-  }
-
-  info(formatter: unknown, ...args: Array<unknown>): void {
-    this.log(LogSeverity.INFO, formatter, ...args);
-  }
-
-  warn(formatter: unknown, ...args: Array<unknown>): void {
-    this.log(LogSeverity.WARN, formatter, ...args);
-  }
-
-  debug(formatter: unknown, ...args: Array<unknown>): void {
-    this.log(LogSeverity.DEBUG, formatter, ...args);
-  }
-
-  error(formatter: unknown, ...args: Array<unknown>): void {
-    this.log(LogSeverity.ERROR, formatter, ...args);
-  }
-
-  log(logSeverity: LogSeverity, formatter: unknown, ...args: Array<unknown>): void {
-    if (!Logger4Node.isLogEnabled(logSeverity)) {
-      return;
-    }
-    this._debugLogger(`${logSeverity} ${formatter}`, ...args);
-  }
-
-  private constructor(name: string) {
-    this._debugLogger = debug(name);
+  instance(name: string): Logger {
+    return new Logger(`${this._applicationName}:${name}`);
   }
 }
