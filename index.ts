7b052b1e8802561947197626d886060ae23fa0d2
Yogendra Singh <yog27ray@gmail.com>
2020-12-27 03:28:19 +0530

    logger

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { LogSeverity } from 'src/logger';
+import { LogSeverity } from './src/logger';
 import { Logger4Node } from './src/logger4-node';
 
 export { Logger4Node, LogSeverity };
--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,11 @@
-import debug, { Debugger } from 'debug';
+import util from 'util';
 
 export const enum LogSeverity {
-    VERBOSE = 'verbose',
-    INFO = 'info',
-    WARN = 'warn',
-    DEBUG = 'debug',
-    ERROR = 'error',
+  VERBOSE = 'verbose',
+  INFO = 'info',
+  WARN = 'warn',
+  DEBUG = 'debug',
+  ERROR = 'error',
 }
 
 const LogLevel: { [key in LogSeverity]: number } = {
@@ -16,53 +16,95 @@ const LogLevel: { [key in LogSeverity]: number } = {
   error: 5,
 };
 
+const Color: { [key in LogSeverity]: string; } & { reset: string; application: string; severity: string; } = {
+  severity: '\x1b[33m', // yellow
+  application: '\x1b[36m', // cyan
+  reset: '\x1b[0m', // reset
+  verbose: '\x1b[37m', // white
+  info: '\x1b[35m', // magenta
+  warn: '\x1b[33m', // yellow
+  debug: '\x1b[34m', // blue
+  error: '\x1b[31m', // Red
+};
+
+const matches: Array<string> = [];
+const doesNotMatches: Array<string> = [];
+(process.env.DEBUG || '*').split(',').forEach((key_: string) => {
+  let key = key_;
+  let operator = '+';
+  if (key[0] === '-') {
+    operator = '-';
+    key = key.substr(1, key.length);
+  }
+  key = key.replace(new RegExp('\\*', 'g'), '.*');
+  switch (operator) {
+    case '-': {
+      doesNotMatches.push(key);
+      return;
+    }
+    default: {
+      matches.push(key);
+    }
+  }
+});
+
+console.log('!!', matches);
+console.log('!!a', doesNotMatches);
 export class Logger {
-    private static LOG_LEVEL_ENABLED: Array<LogSeverity> = [
-      LogSeverity.VERBOSE,
-      LogSeverity.INFO,
-      LogSeverity.WARN,
-      LogSeverity.DEBUG,
-      LogSeverity.ERROR,
-    ].filter((logLevel: LogSeverity) => (LogLevel[process.env.DEBUG_LEVEL] || LogLevel[LogSeverity.DEBUG]) <= LogLevel[logLevel]);
+  private static LOG_LEVEL_ENABLED: Array<LogSeverity> = [
+    LogSeverity.VERBOSE,
+    LogSeverity.INFO,
+    LogSeverity.WARN,
+    LogSeverity.DEBUG,
+    LogSeverity.ERROR,
+  ].filter((logLevel: LogSeverity) => (LogLevel[process.env.DEBUG_LEVEL] || LogLevel[LogSeverity.DEBUG]) <= LogLevel[logLevel]);
 
-    private readonly _debugLogger: Debugger;
+  private enabled: boolean = false;
 
-    private static isLogEnabled(logSeverity: LogSeverity): boolean {
-      return Logger.LOG_LEVEL_ENABLED.includes(logSeverity);
+  static matches(value: string): boolean {
+    return matches.every((pattern: string) => new RegExp(`^${pattern}$`).test(value));
+  }
+
+  static doesNotMatches(value: string): boolean {
+    return doesNotMatches.some((pattern: string) => new RegExp(`^${pattern}$`).test(value));
+  }
+
+  private static isLogEnabled(logSeverity: LogSeverity): boolean {
+    return Logger.LOG_LEVEL_ENABLED.includes(logSeverity);
+  }
+
+  verbose(formatter: unknown, ...args: Array<unknown>): void {
+    this.log(LogSeverity.VERBOSE, formatter, ...args);
+  }
+
+  info(formatter: unknown, ...args: Array<unknown>): void {
+    this.log(LogSeverity.INFO, formatter, ...args);
+  }
+
+  warn(formatter: unknown, ...args: Array<unknown>): void {
+    this.log(LogSeverity.WARN, formatter, ...args);
+  }
+
+  debug(formatter: unknown, ...args: Array<unknown>): void {
+    this.log(LogSeverity.DEBUG, formatter, ...args);
+  }
+
+  error(formatter: unknown, ...args: Array<unknown>): void {
+    this.log(LogSeverity.ERROR, formatter, ...args);
+  }
+
+  log(logSeverity: LogSeverity, formatter: unknown, ...args: Array<unknown>): void {
+    if (!Logger.isLogEnabled(logSeverity) || !this.enabled) {
+      return;
     }
+    console.log(
+      Color.severity, logSeverity,
+      Color.application, this.name,
+      Color[logSeverity], util.format(formatter, ...args),
+      Color.reset);
+  }
 
-    get debugLogger(): Debugger {
-      return this._debugLogger;
-    }
-
-    verbose(formatter: unknown, ...args: Array<unknown>): void {
-      this.log(LogSeverity.VERBOSE, formatter, ...args);
-    }
-
-    info(formatter: unknown, ...args: Array<unknown>): void {
-      this.log(LogSeverity.INFO, formatter, ...args);
-    }
-
-    warn(formatter: unknown, ...args: Array<unknown>): void {
-      this.log(LogSeverity.WARN, formatter, ...args);
-    }
-
-    debug(formatter: unknown, ...args: Array<unknown>): void {
-      this.log(LogSeverity.DEBUG, formatter, ...args);
-    }
-
-    error(formatter: unknown, ...args: Array<unknown>): void {
-      this.log(LogSeverity.ERROR, formatter, ...args);
-    }
-
-    log(logSeverity: LogSeverity, formatter: unknown, ...args: Array<unknown>): void {
-      if (!Logger.isLogEnabled(logSeverity)) {
-        return;
-      }
-      this._debugLogger(formatter, ...args, logSeverity);
-    }
-
-    constructor(name: string) {
-      this._debugLogger = debug(name);
-    }
+  constructor(private name: string) {
+    this.enabled = Logger.matches(name) && !Logger.doesNotMatches(name);
+  }
 }
