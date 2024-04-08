cf85544b165bd6d897445757e9ea1ed4d197aabc
Yogendra Singh <yog27ray@gmail.com>
2024-04-08 17:08:56 +0530

    v1.0.5

--- src/test.spec.js
+++ src/test.spec.js
@@ -41,11 +41,11 @@ describe('Logger4node', () => {
         it('should print all logs', () => {
             printLogsInDifferentLevel(logger1Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(5);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mverbose\x1B[36mLogger1:Instance1\x1B[37mVerbose log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33minfo\x1B[36mLogger1:Instance1\x1B[35mInfo log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(2).args.join('')).to.equal('\x1B[33mwarn\x1B[36mLogger1:Instance1\x1B[33mWarn log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(3).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(4).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mError log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Verbose Logger1:Instance1 Verbose log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Info Logger1:Instance1 Info log');
+            (0, chai_1.expect)(callbackSpy.getCall(2).args.join(' ')).to.equal('Warn Logger1:Instance1 Warn log');
+            (0, chai_1.expect)(callbackSpy.getCall(3).args.join(' ')).to.equal('Debug Logger1:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(4).args.join(' ')).to.equal('Error Logger1:Instance1 Error log');
         });
         it('should not print logger2 logs', () => {
             printLogsInDifferentLevel(logger2Instance1);
@@ -55,55 +55,55 @@ describe('Logger4node', () => {
             logger4_node_1.Logger4Node.setLogPattern('Logger1:*,Logger2:*');
             printLogsInDifferentLevel(logger2Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(5);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mverbose\x1B[36mLogger2:Instance1\x1B[37mVerbose log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33minfo\x1B[36mLogger2:Instance1\x1B[35mInfo log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(2).args.join('')).to.equal('\x1B[33mwarn\x1B[36mLogger2:Instance1\x1B[33mWarn log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(3).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger2:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(4).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger2:Instance1\x1B[31mError log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Verbose Logger2:Instance1 Verbose log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Info Logger2:Instance1 Info log');
+            (0, chai_1.expect)(callbackSpy.getCall(2).args.join(' ')).to.equal('Warn Logger2:Instance1 Warn log');
+            (0, chai_1.expect)(callbackSpy.getCall(3).args.join(' ')).to.equal('Debug Logger2:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(4).args.join(' ')).to.equal('Error Logger2:Instance1 Error log');
         });
-        it('should print only Logger1 debug and above logs', () => {
+        it('should print only Logger1 Debug  and above logs', () => {
             logger4_node_1.Logger4Node.setLogLevel("debug" /* LogSeverity.DEBUG */);
             printLogsInDifferentLevel(logger1Instance1);
             printLogsInDifferentLevel(logger2Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(2);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mError log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Debug Logger1:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Error Logger1:Instance1 Error log');
         });
-        it('should print only Logger1 debug and above logs and logger2 only debug', () => {
+        it('should print only Logger1 Debug  and above logs and logger2 only Debug ', () => {
             logger4_node_1.Logger4Node.setLogLevel("debug" /* LogSeverity.DEBUG */);
             logger4_node_1.Logger4Node.setLogSeverityPattern("debug" /* LogSeverity.DEBUG */, 'Logger2:*');
             printLogsInDifferentLevel(logger1Instance1);
             printLogsInDifferentLevel(logger2Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(3);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mError log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(2).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger2:Instance1\x1B[34mDebug log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Debug Logger1:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Error Logger1:Instance1 Error log');
+            (0, chai_1.expect)(callbackSpy.getCall(2).args.join(' ')).to.equal('Debug Logger2:Instance1 Debug log');
         });
         it('should print both instance of Logger1', () => {
             printLogsInDifferentLevel(logger1Instance1);
             printLogsInDifferentLevel(logger1Instance2);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(10);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mverbose\x1B[36mLogger1:Instance1\x1B[37mVerbose log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33minfo\x1B[36mLogger1:Instance1\x1B[35mInfo log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(2).args.join('')).to.equal('\x1B[33mwarn\x1B[36mLogger1:Instance1\x1B[33mWarn log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(3).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(4).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mError log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(5).args.join('')).to.equal('\x1B[33mverbose\x1B[36mLogger1:Instance2\x1B[37mVerbose log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(6).args.join('')).to.equal('\x1B[33minfo\x1B[36mLogger1:Instance2\x1B[35mInfo log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(7).args.join('')).to.equal('\x1B[33mwarn\x1B[36mLogger1:Instance2\x1B[33mWarn log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(8).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance2\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(9).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance2\x1B[31mError log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Verbose Logger1:Instance1 Verbose log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Info Logger1:Instance1 Info log');
+            (0, chai_1.expect)(callbackSpy.getCall(2).args.join(' ')).to.equal('Warn Logger1:Instance1 Warn log');
+            (0, chai_1.expect)(callbackSpy.getCall(3).args.join(' ')).to.equal('Debug Logger1:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(4).args.join(' ')).to.equal('Error Logger1:Instance1 Error log');
+            (0, chai_1.expect)(callbackSpy.getCall(5).args.join(' ')).to.equal('Verbose Logger1:Instance2 Verbose log');
+            (0, chai_1.expect)(callbackSpy.getCall(6).args.join(' ')).to.equal('Info Logger1:Instance2 Info log');
+            (0, chai_1.expect)(callbackSpy.getCall(7).args.join(' ')).to.equal('Warn Logger1:Instance2 Warn log');
+            (0, chai_1.expect)(callbackSpy.getCall(8).args.join(' ')).to.equal('Debug Logger1:Instance2 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(9).args.join(' ')).to.equal('Error Logger1:Instance2 Error log');
         });
         it('should print only instance1 of Logger1', () => {
             logger4_node_1.Logger4Node.setLogPattern('Logger1:*,-Logger1:Instance2*');
             printLogsInDifferentLevel(logger1Instance1);
             printLogsInDifferentLevel(logger1Instance2);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(5);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to.equal('\x1B[33mverbose\x1B[36mLogger1:Instance1\x1B[37mVerbose log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(1).args.join('')).to.equal('\x1B[33minfo\x1B[36mLogger1:Instance1\x1B[35mInfo log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(2).args.join('')).to.equal('\x1B[33mwarn\x1B[36mLogger1:Instance1\x1B[33mWarn log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(3).args.join('')).to.equal('\x1B[33mdebug\x1B[36mLogger1:Instance1\x1B[34mDebug log\x1B[0m');
-            (0, chai_1.expect)(callbackSpy.getCall(4).args.join('')).to.equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mError log\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to.equal('Verbose Logger1:Instance1 Verbose log');
+            (0, chai_1.expect)(callbackSpy.getCall(1).args.join(' ')).to.equal('Info Logger1:Instance1 Info log');
+            (0, chai_1.expect)(callbackSpy.getCall(2).args.join(' ')).to.equal('Warn Logger1:Instance1 Warn log');
+            (0, chai_1.expect)(callbackSpy.getCall(3).args.join(' ')).to.equal('Debug Logger1:Instance1 Debug log');
+            (0, chai_1.expect)(callbackSpy.getCall(4).args.join(' ')).to.equal('Error Logger1:Instance1 Error log');
         });
         afterEach(() => {
             callbackSpy.restore();
@@ -131,14 +131,14 @@ describe('Logger4node', () => {
         it('should print logs not only in string', () => {
             printLogsInDifferentType(logger1Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(1);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to
-                .equal('\x1B[33merror\x1B[36mLogger1:Instance1\x1B[31mthis is  1 true { key1: 1, value: 2 }\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to
+                .equal('Error Logger1:Instance1 this is  1 true { key1: 1, value: 2 }');
         });
         it('should print logs only in string', () => {
             printLogsInDifferentType(logger2Instance1);
             (0, chai_1.expect)(callbackSpy.callCount).to.equal(1);
-            (0, chai_1.expect)(callbackSpy.getCall(0).args.join('')).to
-                .equal('\x1B[33merror\x1B[36mLogger2:Instance1\x1B[31mthis is  1 true {"key1":1,"value":2}\x1B[0m');
+            (0, chai_1.expect)(callbackSpy.getCall(0).args.join(' ')).to
+                .equal('Error Logger2:Instance1 this is  1 true {"key1":1,"value":2}');
         });
         afterEach(() => {
             callbackSpy.restore();
