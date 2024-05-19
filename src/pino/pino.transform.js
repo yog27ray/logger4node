49537c9c60010257b281cee658ab5080bd0b82cb
Yogendra Singh <yog27ray@gmail.com>
2024-05-19 18:16:53 +0530

    v1.0.26

---
+++ src/pino/pino.transform.js
@@ -0,0 +1,19 @@
+﻿const build = require('pino-abstract-transport');
+const { pipeline, Transform } = require('stream');
+
+module.exports = function (options) {
+  return build(function (source) {
+    const myTransportStream = new Transform({
+      autoDestroy: true,
+      objectMode: true,
+      transform(chunk, enc, cb) {
+        chunk.message = chunk.msg;
+        delete chunk.msg;
+        this.push(`${JSON.stringify(chunk)}\n`);
+        cb();
+      },
+    });
+    pipeline(source, myTransportStream, () => {});
+    return myTransportStream;
+  }, { enablePipelining: true });
+}
