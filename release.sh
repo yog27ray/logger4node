95bded7c1651259fabf1381fc68c1338db8db9a5
Yogendra Singh <yog27ray@gmail.com>
2024-07-02 10:00:18 +0530

    doc: README.md update

--- release.sh
+++ release.sh
@@ -37,7 +37,7 @@ git tag -a "$TAG" -m "$RELEASE_NAME"
 git push release "$TAG"
 gh release create "$TAG" --title "$RELEASE_NAME" --notes "$RELEASE_BODY"
 
-sed -i 's/"name": "logger4node"/"name": "@yog27ray\/logger4node"/g' package.json
-sed -i 's/"name": "logger4node"/"name": "@yog27ray\/logger4node"/g' package-lock.json
+sed -i '' 's/"name": "logger4node"/"name": "@yog27ray\/logger4node"/g' package.json
+sed -i '' 's/"name": "logger4node"/"name": "@yog27ray\/logger4node"/g' package-lock.json
 npm publish --registry=https://npm.pkg.github.com/  --access=public
 echo "Release $RELEASE_NAME created successfully!"
