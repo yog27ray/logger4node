565e979de18ed12b0d81ba9c3c3dd9c92e1d1143
yog27ray <yog27ray@gmail.com>
2024-11-18 15:59:46 +0530

    v1.0.47

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { GithubConfig, Logger, LogSeverity } from './src/logger/logger';
-import { Logger4Node } from './src/logger/logger4-node';
-
-export { GithubConfig, Logger, Logger4Node, LogSeverity };
