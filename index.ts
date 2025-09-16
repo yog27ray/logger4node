bddbe813ed3a78d69dec7c70e69a022605b258fd
yog27ray <yog27ray@gmail.com>
2025-09-16 11:16:17 +0530

    fix: update library

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { GithubConfig, Logger, LogSeverity } from './src/logger/logger';
 import { Logger4Node } from './src/logger/logger4-node';
 
-export { GithubConfig, Logger, Logger4Node, LogSeverity };
+export type { GithubConfig };
+export { Logger, Logger4Node, LogSeverity };
